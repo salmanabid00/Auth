570fda97a4ed8175d0f5e522d272368542438c28
Salman Abid <167421596+salmanabid00@users.noreply.github.com>
2026-05-14 12:12:08 +0500

    Refactor welcome message and logout URL

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+const pageParams = new URLSearchParams(window.location.search);
+
 var user = localStorage.getItem("loggedInUser");
 
 if (user) {
   var parsedUser = JSON.parse(user);
-  document.getElementById("welcomMessage").innerHTML = `Welcome, ${parsedUser.name}!`;
+  const displayName = parsedUser.name;
+
+  document.getElementById("welcomMessage").innerHTML = `Welcome, ${displayName}!`;
   document.getElementById("LogoutButton").classList.remove("hidden");
 } else {
   document.getElementById("welcomMessage").innerHTML = `Hello, please <a href="login.html">Login</a>`;
@@ -11,5 +15,6 @@ if (user) {
 
 document.getElementById("LogoutButton").addEventListener("click", function () {
   localStorage.removeItem("loggedInUser");
-  window.location.href = "login.html";
+
+  window.location.href = "login.html?logout=true";
 });
